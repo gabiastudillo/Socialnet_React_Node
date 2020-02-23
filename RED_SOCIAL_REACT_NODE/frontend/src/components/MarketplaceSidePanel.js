79b535fd346b4c85f59c5f364747cbbf3c24e06e
Gabriela Astudillo <35819215+gabiastudillo@users.noreply.github.com>
2020-02-23 16:00:31 -0500

    Modificación del SidePanel del Marketplace

--- RED_SOCIAL_REACT_NODE/frontend/src/components/MarketplaceSidePanel.js
+++ RED_SOCIAL_REACT_NODE/frontend/src/components/MarketplaceSidePanel.js
@@ -5,20 +5,24 @@ export default class MarketplaceSidePanel extends Component {
     render() {
         return (
             <nav className="nav col flex-column">
-                <Link className="nav-link btn-lg text-lg-left m-2 my-3" to="/marketplace"><span><i className="fas fa-store fa-lg mr-3"></i>Marketplace</span></Link>
-                <Link className="btn btn-primary ml-4" to="/producto" role="button">+ Vender algo</Link>
-                <h6 className="m-4 mt-5">Categorías</h6>
+                <Link className="nav-link btn-lg text-lg-left my-2" to="/marketplace"><span><i className="fas fa-store fa-lg mr-3"></i>Marketplace</span></Link>
+                <Link className="btn btn-primary mt-3 ml-3" to="/producto" role="button">+ Vender algo</Link>
+                <h6 className="ml-3 mt-3">Ubicación</h6>
+                <div className="form-group ml-3">
+                    <input type="text" className="form-control" placeholder="Ubicación" />
+                </div>
+                <h6 className="m-3">Categorías</h6>
                 <Link className="nav-link" to="google.com"><i className="fas fa-building ml-1 mr-3"></i> Alquileres</Link>
                 <Link className="nav-link" to="google.com"><i className="fas fa-tshirt mr-3"></i>Ropa y accesorios</Link>
                 <Link className="nav-link" to="google.com"><i className="fas fa-home ml-1 mr-3"></i>Casa y jardín</Link>
-                <Link className="nav-link" to="google.com"><i className="fas fa-car ml-1 mr-3"></i> Vehículos</Link>                        
+                <Link className="nav-link" to="google.com"><i className="fas fa-car ml-1 mr-3"></i> Vehículos</Link>
                 <Link className="nav-link" to="google.com"><i className="fas fa-mobile-alt ml-2 mr-3"></i> Electrónica</Link>
                 <Link className="nav-link" to="google.com"><i className="fas fa-heart ml-1 mr-3"></i>Familia</Link>
                 <Link className="nav-link" to="google.com"><i className="fas fa-guitar ml-1 mr-3"></i>Pasatiempos</Link>
                 <Link className="nav-link" to="google.com"><i className="fas fa-binoculars ml-1 mr-3"></i>Clasificados</Link>
                 <Link className="nav-link" to="google.com"><i className="fas fa-home ml-1 mr-3"></i>Viviendas en venta</Link>
                 <Link className="nav-link" to="google.com"><i className="fas fa-gamepad ml-1 mr-3"></i>Entretenimiento</Link>
-                                
+
             </nav>
 
         )

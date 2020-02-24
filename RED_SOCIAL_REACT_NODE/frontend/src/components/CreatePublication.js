994fa5f30f26a36d3ee793b049808808a84d5a0a
Gabriela Astudillo <35819215+gabiastudillo@users.noreply.github.com>
2020-02-24 02:43:30 -0500

    Ajustes al formulario Crear publicación

--- RED_SOCIAL_REACT_NODE/frontend/src/components/CreatePublication.js
+++ RED_SOCIAL_REACT_NODE/frontend/src/components/CreatePublication.js
@@ -22,7 +22,7 @@ export default class CreatePublication extends Component {
                         <img src={'https://laboratoriosniam.com/wp-content/uploads/2018/07/michael-dam-258165-unsplash_WEB2.jpg'} width="45" height="45" className="rounded-circle m-2 float-left border" alt="" />
                         <div className="m-0 p-0 w-100">
                             <div className="form-group m-0 p-0 w-100">
-                                <textarea className="form-control w-100 font-weight-normal m-0 pt-3 border-0" rows="3" placeholder="¿Qué estás pensando, Gabriela?"></textarea>
+                                <textarea className="form-control w-100 font-weight-normal m-0 pt-3 border-0 mitextarea" rows="2" placeholder="¿Qué estás pensando, Gabriela?"></textarea>
                             </div>
                         </div>
                     </div>
@@ -36,8 +36,8 @@ export default class CreatePublication extends Component {
                         <button type="button" className="btn bg-light rounded-pill">Etiquetar</button>
                     </div>
                 </div>
-                <div className="card-footer p-3">
-                    <div className="row justify-content-around pt-1 pb-3">
+                <div className="card-footer p-2 px-3">
+                    <div className="row justify-content-around pb-2">
                         <div className="col-4">
                             <div className="custom-control custom-checkbox custom-control-inline">
                                 <input type="checkbox" className="custom-control-input" id="customCheck1" />
@@ -51,7 +51,7 @@ export default class CreatePublication extends Component {
                             </div>
                         </div>
                     </div>
-                    <button type="button" className="btn btn-primary w-100">Publicar</button>
+                    <button type="button" className="btn btn-primary p-1 w-100">Publicar</button>
                 </div>
             </div>
         )

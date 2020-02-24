5bba9dcfdb6eb69d4f90a494763d58d2883ca6c6
Gabriela Astudillo <35819215+gabiastudillo@users.noreply.github.com>
2020-02-24 01:07:41 -0500

    Modificación del formulario para crear una publicación

--- RED_SOCIAL_REACT_NODE/frontend/src/components/CreatePublication.js
+++ RED_SOCIAL_REACT_NODE/frontend/src/components/CreatePublication.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 export default class CreatePublication extends Component {
-    
-    state={
-        users:[]
+
+    state = {
+        users: []
     }
 
-    async componentDidMount(){
-        const res=axios.get('http://localhost:4000/api/publicaciones');
-     console.log(res);
+    async componentDidMount() {
+        const res = axios.get('http://localhost:4000/api/publicaciones');
+        console.log(res);
     }
-    
+
     render() {
         return (
-            <div className="card m-2">
+            <div className="card mx-2 my-2">
                 <h6 className="card-header">Crear publicación</h6>
                 <div className="form-inline">
 
@@ -22,18 +22,36 @@ export default class CreatePublication extends Component {
                         <img src={'https://laboratoriosniam.com/wp-content/uploads/2018/07/michael-dam-258165-unsplash_WEB2.jpg'} width="45" height="45" className="rounded-circle m-2 float-left border" alt="" />
                         <div className="m-0 p-0 w-100">
                             <div className="form-group m-0 p-0 w-100">
-                                <textarea className="form-control w-100 text-muted font-weight-normal m-0 pt-3 border-0" rows="3" placeholder="¿Qué estás pensando, Gabriela?"></textarea>
+                                <textarea className="form-control w-100 font-weight-normal m-0 pt-3 border-0" rows="3" placeholder="¿Qué estás pensando, Gabriela?"></textarea>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="card-footer bg-transparent d-flex btn-group-sm" role="group">
-                    <button type="button" className="btn bg-light w-100 rounded-pill mr-3"><label className="font-weight-bold"></label>Foto/video</button>
-                    <button type="button" className="btn bg-light w-100 rounded-pill mr-3"><label className="font-weight-bold"></label>Etiquetar am...</button>
-                    <button type="button" className="btn bg-light w-100 rounded-pill mr-3"><label className="font-weight-bold"></label>Sentimiento/...</button>
-                    <button type="button" className="btn bg-light w-100 rounded-pill"><label className="font-weight-bold"></label>Más...</button>
-
+                <div className="card-body border-top p-2">
+                    <div className="row justify-content-between mx-2">
+                        <button type="button" className="btn bg-light rounded-pill">Foto/video</button>                        
+                        <button type="button" className="btn bg-light rounded-pill">Archivo</button>
+                        <button type="button" className="btn bg-light rounded-pill">Encuesta</button>                        
+                        <button type="button" className="btn bg-light rounded-pill">Etiquetar</button>
+                    </div>
+                </div>
+                <div className="card-footer p-3">
+                    <div className="row justify-content-around pt-1 pb-3">
+                        <div className="col-4">
+                            <div className="custom-control custom-checkbox custom-control-inline">
+                                <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                                <label className="custom-control-label" htmlFor="customCheck1">Noticias</label>
+                            </div>
+                        </div>
+                        <div className="col-4">
+                            <div className="custom-control custom-checkbox custom-control-inline">
+                                <input type="checkbox" className="custom-control-input" id="customCheck2" />
+                                <label className="custom-control-label" htmlFor="customCheck2">Tu historia</label>
+                            </div>
+                        </div>
+                    </div>
+                    <button type="button" className="btn btn-primary w-100">Publicar</button>
                 </div>
             </div>
         )

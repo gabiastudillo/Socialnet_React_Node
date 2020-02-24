ef0280a511742c7dd2b54f16e5e3e89610e29309
Gabriela Astudillo <35819215+gabiastudillo@users.noreply.github.com>
2020-02-23 22:28:42 -0500

    Portada, foto de perfil, menu de perfil

--- RED_SOCIAL_REACT_NODE/frontend/src/components/Perfil.js
+++ RED_SOCIAL_REACT_NODE/frontend/src/components/Perfil.js
@@ -1,23 +1,43 @@
 import React, { Component } from 'react'
 
 import axios from 'axios'
+import { Link } from 'react-router-dom';
 
 export default class Perfil extends Component {
-    state={
-        users:[]
+    state = {
+        users: []
     }
 
-    async componentDidMount(){
-        const res=axios.get('http:/localhost:4000/user/1');
-        this.setState({users:res.data});
+    async componentDidMount() {
+        const res = axios.get('http:/localhost:4000/user/1');
+        this.setState({ users: res.data });
         console.log(this.state.users.data);
     }
+
     render() {
         return (
-            <div>
-                .col-md-2
-                div.col
+            <div className="container col-7">
+                <div className="card text-white">
+                    <img src="https://k37.kn3.net/9E0D271BA.jpg" className="card-body-img" alt="..." />
+                    <div className="card-img-overlay">
+                        <div className="media h-100">
+                            <img src={'https://laboratoriosniam.com/wp-content/uploads/2018/07/michael-dam-258165-unsplash_WEB2.jpg'} width="180" height="180" className="align-self-end rounded-circle mr-2 mt-2 border" alt="" />
+                            <div className="media-body align-self-end ">
+                                <h2 className="mt-0 p-3 mb-5">Gabriela Astudillo</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body my-0 py-3 ml-auto">
+                        <Link className="font-weight-bold p-3 m-0 border-right" to="/">Biografía</Link>
+                        <Link className="font-weight-bold p-3 m-0 border-right">Información</Link>
+                        <Link className="font-weight-bold p-3 m-0 border-right">Amigos</Link>
+                        <Link className="font-weight-bold p-3 m-0 border-right">Fotos</Link>
+                        <Link className="font-weight-bold p-3 m-0 border-right">Videos</Link>
+                        <Link className="font-weight-bold p-3 m-0 border-right">Archivo</Link>                        
+                        <Link className="font-weight-bold p-3 m-0">Más...</Link>
+                    </div>
 
+                </div>
             </div>
         )
     }
--- RED_SOCIAL_REACT_NODE/frontend/src/components/Publication.js
+++ RED_SOCIAL_REACT_NODE/frontend/src/components/Publication.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import InteraccionPublicacion from './InteraccionPublicacion'
 import SeccionComentarios from './SeccionComentarios'
+import { Link } from 'react-router-dom'
 
 export default class Publication extends Component {
     render() {
@@ -10,7 +11,7 @@ export default class Publication extends Component {
                 <div className="card-body p-3">
                     <img src={'https://laboratoriosniam.com/wp-content/uploads/2018/07/michael-dam-258165-unsplash_WEB2.jpg'} width="45" height="45" className="rounded-circle mr-2 float-left border" alt="" />
                     <div>
-                        <a href="className=#v-pills-home" className="card-title"><strong>Gabriela Astudillo</strong></a>
+                        <Link to="/perfil" className="card-title"><strong>Gabriela Astudillo</strong></Link>
                         <h6 className="text-muted font-weight-normal">1 h</h6>
                     </div>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>

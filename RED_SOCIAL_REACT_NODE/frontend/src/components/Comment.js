401fc5f56c3cf08d0c2066a8ea1e836eae676185
Gabriela Astudillo <35819215+gabiastudillo@users.noreply.github.com>
2020-02-24 01:06:51 -0500

    Modificaciones del botón Eliminar

--- RED_SOCIAL_REACT_NODE/frontend/src/components/Comment.js
+++ RED_SOCIAL_REACT_NODE/frontend/src/components/Comment.js
@@ -13,7 +13,7 @@ export default class Comment extends Component {
                     <div className="media-body ">
                         <div className="container-fluid rounded bg-light m-0 px-3 py-2">
                             <Link className="mt-0 font-weight-bold" to="/perfil">Gabriela Astudillo</Link>
-                            <Link className="btn rounded-pill bg-tranparent border-0 pt-0 pb-2 pl-2 pr-2 ml-2">...</Link>
+                            <button className="btn rounded-pill bg-tranparent border-0 pt-0 pb-2 pl-2 pr-2 ml-2">...</button>
                             <p className="text-justify">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
                             <div className="row m-0 p-0">
                                 <a href="className=#v-pills-home" className="mr-3">Me gusta</a>
--- RED_SOCIAL_REACT_NODE/frontend/src/components/Respuesta.js
+++ RED_SOCIAL_REACT_NODE/frontend/src/components/Respuesta.js
@@ -9,14 +9,13 @@ export default class Respuesta extends Component {
                     <img className="align-self-start mr-2 my-2 rounded-circle border" src={'https://laboratoriosniam.com/wp-content/uploads/2018/07/michael-dam-258165-unsplash_WEB2.jpg'} width="26" height="26" alt=""/>
                     <div className="media-body bg-light rounded px-3 py-2">
                         <Link className="mt-0 font-weight-bold" to="/perfil">Gabriela Astudillo</Link>
-                        <Link className="btn rounded-pill bg-tranparent border-0 pt-0 pb-2 pl-2 pr-2 ml-2">...</Link>
+                        <button className="btn rounded-pill bg-tranparent border-0 pt-0 pb-2 pl-2 pr-2 ml-2">...</button>
                         <p className="text-justify">Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
                         <div className="row m-0 p-0">
                             <a href="className=#v-pills-home" className="mr-3">Me gusta</a>
                             <a href="className=#v-pills-home" className="mr-3">Responder</a>
                             <a href="className=#v-pills-home" className="text-muted">5 min</a>
-                        </div>
-                        
+                        </div>                        
                     </div>
                 </div>
             </li>

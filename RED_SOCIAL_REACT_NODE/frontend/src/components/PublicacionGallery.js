47edc1beaeea1c38509e1e83deb08817757c0ebb
Gabriela Astudillo <35819215+gabiastudillo@users.noreply.github.com>
2020-02-24 20:38:41 -0500

    Creación del componente PublicacionGallery
    Hice este componente para mostrar la gallería de imagenes o videos de una publicación.

---
+++ RED_SOCIAL_REACT_NODE/frontend/src/components/PublicacionGallery.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class PublicacionGallery extends Component {
+    render() {
+        return (
+            <img src="https://www.veintitantos.com/sites/default/files/styles/v2-img1000x563/public/articulo/2019-01/tips_para_conseguir_viajes_mas_baratos_en_2019.jpg?itok=T3m2ixKL" className="img-fluid" alt="" />
+               
+        )
+    }
+}

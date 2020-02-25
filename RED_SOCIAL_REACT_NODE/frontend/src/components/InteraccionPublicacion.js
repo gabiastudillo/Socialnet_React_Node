e1c9e491f7484c2a24fba4ce56a8a3d925085f30
Gabriela Astudillo <35819215+gabiastudillo@users.noreply.github.com>
2020-02-24 20:37:12 -0500

    Espacios en blanco
    Borré los espacios en blanco

--- RED_SOCIAL_REACT_NODE/frontend/src/components/InteraccionPublicacion.js
+++ RED_SOCIAL_REACT_NODE/frontend/src/components/InteraccionPublicacion.js
@@ -5,7 +5,6 @@ export default class InteraccionPublicacion extends Component {
     render() {
         return (
             <ul className="list-group list-group-flush m-0 p-0">
-
             <li className="list-group-item">
                 <div className="row">
                     <a href="className=#v-pills-home" className="text-muted mr-5"><i className="fab fa-gratipay fa-lg mr-2 ml-2"></i>25</a>
@@ -15,7 +14,6 @@ export default class InteraccionPublicacion extends Component {
                     </div>
                 </div>
             </li>
-
             <li className="list-group-item p-1 w-100">
                 <div className="row ml-1 mr-1">
                     <div className="col col-sm-12 col-lg-4 col-md-4 m-0">

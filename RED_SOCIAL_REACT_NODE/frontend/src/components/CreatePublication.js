511060775fef74783872ea49895f326f1dcb6282
Gabriela Astudillo <35819215+gabiastudillo@users.noreply.github.com>
2020-02-24 20:35:52 -0500

    Modificación de las secciones

--- RED_SOCIAL_REACT_NODE/frontend/src/components/CreatePublication.js
+++ RED_SOCIAL_REACT_NODE/frontend/src/components/CreatePublication.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom';
 
 export default class CreatePublication extends Component {
 
@@ -12,47 +13,45 @@ export default class CreatePublication extends Component {
         console.log(res);
     }
 
+    onChangeHandler = event => {
+        console.log(event.target.files[0])
+    }
+    
     render() {
         return (
             <div className="card mx-2 my-2">
                 <h6 className="card-header">Crear publicación</h6>
-                <div className="form-inline">
-
+                <div className="form-inline pb-0">
                     <div className="card-body p-1 m-0 w-100 d-flex">
                         <img src={'https://laboratoriosniam.com/wp-content/uploads/2018/07/michael-dam-258165-unsplash_WEB2.jpg'} width="45" height="45" className="rounded-circle m-2 float-left border" alt="" />
                         <div className="m-0 p-0 w-100">
                             <div className="form-group m-0 p-0 w-100">
-                                <textarea className="form-control w-100 font-weight-normal m-0 pt-3 border-0 mitextarea" rows="2" placeholder="¿Qué estás pensando, Gabriela?"></textarea>
+                                <textarea className="form-control w-100 font-weight-normal m-0 pt-3 pb-0 border-0 mitextarea" rows="2" placeholder="¿Qué estás pensando, Gabriela?"></textarea>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="card-body border-top p-2">
-                    <div className="row justify-content-between mx-2">
-                        <button type="button" className="btn bg-light rounded-pill">Foto/video</button>                        
-                        <button type="button" className="btn bg-light rounded-pill">Archivo</button>
-                        <button type="button" className="btn bg-light rounded-pill">Encuesta</button>                        
-                        <button type="button" className="btn bg-light rounded-pill">Etiquetar</button>
-                    </div>
-                </div>
-                <div className="card-footer p-2 px-3">
-                    <div className="row justify-content-around pb-2">
-                        <div className="col-4">
-                            <div className="custom-control custom-checkbox custom-control-inline">
-                                <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                                <label className="custom-control-label" htmlFor="customCheck1">Noticias</label>
+                <div className="card-body p-0 m-0">
+                    <ul className="list-group list-group-flush m-0">
+                        <li className="list-group-item px-2 py-1 w-100">
+                           
+
+
+                           
+                        </li>
+
+                        <li className="list-group-item rounded py-1 w-100">
+                            <div className="row justify-content-between px-2 ">
+                                <Link  className="btn bg-light rounded-pill m-1"><i className="fas fa-image mr-2"></i>Foto/video</Link>
+                                <button type="button" className="btn bg-light rounded-pill m-1"><i class="fas fa-file-upload mr-2"></i>Archivo</button>
+                                <button type="button" className="btn bg-light rounded-pill m-1"><i class="fas fa-poll mr-2"></i>Encuesta</button>
+                                <button type="button" className="btn bg-light rounded-pill m-1"><i class="fas fa-user-plus mr-2"></i>Etiquetar</button>
                             </div>
-                        </div>
-                        <div className="col-4">
-                            <div className="custom-control custom-checkbox custom-control-inline">
-                                <input type="checkbox" className="custom-control-input" id="customCheck2" />
-                                <label className="custom-control-label" htmlFor="customCheck2">Tu historia</label>
-                            </div>
-                        </div>
-                    </div>
-                    <button type="button" className="btn btn-primary p-1 w-100">Publicar</button>
+                        </li>
+                    </ul>
                 </div>
+                
             </div>
         )
     }

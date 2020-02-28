0b908dfcaf056b15223a9632ad2e89af8518e6ae
Gabriela Astudillo <35819215+gabiastudillo@users.noreply.github.com>
2020-02-28 00:36:16 -0500

    Corrección del placeholder

--- RED_SOCIAL_REACT_NODE/frontend/src/components/CreateRespuesta.js
+++ RED_SOCIAL_REACT_NODE/frontend/src/components/CreateRespuesta.js
@@ -7,7 +7,7 @@ export default class createRespuesta extends Component {
             <img src={'https://laboratoriosniam.com/wp-content/uploads/2018/07/michael-dam-258165-unsplash_WEB2.jpg'} width="26" height="26" className="rounded-circle float-left mr-2 mt-2 border" alt="" />
             <div className="col m-0 p-0">
                 <div className="input-group-prepend border rounded-pill bg-light">
-                    <input type="text" className="form-control rounded-pill border-0 bg-light" placeholder="Escribe un comentario..." aria-label="Caja  para comentario" />
+                    <input type="text" className="form-control rounded-pill border-0 bg-light" placeholder="Escribe una respuesta..." aria-label="Caja  para comentario" />
                     <div className="input-group-append">
                         <button className="btn btn-outline-secondary-flush rounded-circle pl-2 pr-2" type="button" data-toggle="tooltip" data-placement="top" title="Inserta un emoji"><i className="far fa-smile"></i></button>
                         <button className="btn btn-outline-secondary-flush rounded-circle pl-2 pr-2" type="button" data-toggle="tooltip" data-placement="top" title="Adjunta una foto o un video"><i className="far fa-file-image"></i></button>

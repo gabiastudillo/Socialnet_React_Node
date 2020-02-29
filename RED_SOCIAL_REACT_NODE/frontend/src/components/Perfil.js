dea7e1998cefe8ae116ce8e058c819816072e6a4
Gabriela Astudillo <35819215+gabiastudillo@users.noreply.github.com>
2020-02-29 02:46:53 -0500

    ajustes

--- RED_SOCIAL_REACT_NODE/frontend/src/components/Perfil.js
+++ RED_SOCIAL_REACT_NODE/frontend/src/components/Perfil.js
@@ -21,20 +21,21 @@ export default class Perfil extends Component {
                     <img src="https://k37.kn3.net/9E0D271BA.jpg" className="card-body-img" alt="..." />
                     <div className="card-img-overlay">
                         <div className="media h-100">
-                            <img src={'https://laboratoriosniam.com/wp-content/uploads/2018/07/michael-dam-258165-unsplash_WEB2.jpg'} width="180" height="180" className="align-self-end rounded-circle mr-2 mt-2 img-rounded thumbnail" alt="" />
+                            <img src={'https://laboratoriosniam.com/wp-content/uploads/2018/07/michael-dam-258165-unsplash_WEB2.jpg'} width="180em" height="180em" className="align-self-end rounded-circle mr-2 mt-2 img-rounded thumbnail" alt="" />
                             <div className="media-body align-self-end ">
                                 <h4 className="mt-0 p-3 mb-5">Gabriela Astudillo</h4>
                             </div>
                         </div>
                     </div>
-                    <div className="card-body my-0 py-3 ml-auto">
-                        <Link className="font-weight-bold p-3 m-0 border-right" to="/">Biografía</Link>
-                        <Link className="font-weight-bold p-3 m-0 border-right" to="/">Información</Link>
-                        <Link className="font-weight-bold p-3 m-0 border-right" to="/">Amigos</Link>
-                        <Link className="font-weight-bold p-3 m-0 border-right" to="/">Fotos</Link>
-                        <Link className="font-weight-bold p-3 m-0 border-right" to="/">Videos</Link>
-                        <Link className="font-weight-bold p-3 m-0 border-right" to="/">Archivo</Link>                        
-                        <Link className="font-weight-bold p-3 m-0" to="/">Más...</Link>
+                    <div className="card-body my-0 py-3 ml-auto justify-content-between">
+                        <div className="btn-group pl-5 btns m-0 rounded-0 w-100" role="group" aria-label="First group">
+                            <Link className="btn font-weight-bold border-right" to="/">Biografía</Link>
+                            <Link className="btn font-weight-bold border-right" to="/">Información</Link>
+                            <Link className="btn font-weight-bold border-right" to="/">Amigos</Link>
+                            <Link className="btn font-weight-bold border-right" to="/">Fotos</Link>
+                            <Link className="btn font-weight-bold border-right" to="/">Videos</Link>
+                            <Link className="btn font-weight-bold border-right" to="/">Archivo</Link>
+                        </div>
                     </div>
 
                 </div>

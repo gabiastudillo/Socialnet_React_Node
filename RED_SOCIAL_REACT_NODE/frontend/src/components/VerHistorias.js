e83bf9dc3c68c4b654e1ffed10074ec1e74cb06b
Gabriela Astudillo <35819215+gabiastudillo@users.noreply.github.com>
2020-03-06 23:35:52 -0500

    Carrusel de historias
    Ahora ya funciona el carrusel d elas historias

--- RED_SOCIAL_REACT_NODE/frontend/src/components/VerHistorias.js
+++ RED_SOCIAL_REACT_NODE/frontend/src/components/VerHistorias.js
@@ -12,13 +12,42 @@ export default class VerHistorias extends Component {
                 </div>
 
                 <div className="container-fluid">
-                    <div id="carousel-example" className="carousel slide" data-ride="carousel">
-                        <div className="carousel-inner row w-100 mx-auto" role="listbox">
-                            <Historia />
-                            <Historia />
-                            <Historia />
-                            <Historia />
+
+                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <div className="row">
+                                    <Historia />
+                                    <Historia />
+                                    <Historia />
+                                    <Historia />
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div className="row">
+                                    <Historia />
+                                    <Historia />
+                                    <Historia />                                    
+                                    <Historia />
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div className="row">
+                                    <Historia />
+                                    <Historia />
+                                    <Historia />
+                                    <Historia />
+                                </div>
+                            </div>
                         </div>
+                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
                     </div>
                 </div>
             </div>
